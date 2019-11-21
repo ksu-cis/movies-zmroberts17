8e34f8383f701b6dd59a8fbeb4afca82ba5817cd
Zane Roberts <zmroberts17@ksu.edu>
2019-11-21 14:35:22 -0600

    This should add the client-side

--- Movies/wwwroot/js/site.js
+++ Movies/wwwroot/js/site.js
@@ -2,3 +2,53 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+
+var movieEntries = document.getElementsByClassName("movie-entry");
+
+var form = document.getElementById("search-and-filter-form");
+
+form.addEventListener('submit', function (event)) {
+    event.preventDefault();
+
+    var term = document.getElementById("search").value;
+
+    var mpaa = [];
+    var mpaaCheckBoxes = document.getElementByClassName("mpaa");
+
+    for (var j = 0; j < mpaaCheckBoxes.length; j++) {
+        if (mpaaCheckBoxes[j].checked) {
+            mpaa.push(mpaaCheckBoxes[j].value);
+        }
+    }
+
+    var minIMDB = document.getElementById("minIMDB").value;
+
+    for (i = 0; i < movieEntries.length; i++) {
+        var entry = movieEntries[i];
+
+        // Step 1: Unhide all entries
+        entry.style.display = "block";
+
+        // Step 2: Filter by search term
+        if (term) {
+            entry = movieEntries[i];
+            if (!entry.dataset.title.toLowerCase().includes(term.toLowerCase())) {
+                entry.style.display = "none";
+            }
+        }
+
+        // Step 3: Filter by mpaa rating
+        if (mpaa.length > 0) {
+            if (!mpaa.includes(entry.dataset.mpaa)) {
+                entry.style.display = "none";
+            }
+        }
+
+        // Step 4: Filter by imdb rating
+        if (minIMDB) {
+            if (!entry.dataset.imdb || parseFloat(minIMDB) > parseFloat(entry.dataset.imdb)) {
+                entry.style.display("none");
+            }
+        }
+    }
+}
